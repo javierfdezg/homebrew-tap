59ebc66b99e585afcb9ea33fb067b109966c5e85
Javier Fernandez <javier.fdez@gmail.com>
2016-04-25 11:39:37 +0200

    New docker-manager version

---
+++ docker-manager.rb
@@ -0,0 +1,26 @@
+class DockerManager < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/javierfdezg/docker-manager/archive/0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 ""
+
+  depends_on 'jq'
+
+  def install
+
+    Dir['lib'].each do |path|
+        app   = File.basename(File.dirname(path))
+        src   = "#{path}/."
+        dest  = "#{lib}/#{app}/"
+
+        print src
+        print dest
+
+        mkdir_p dest
+        cp_r src, dest
+    end
+
+    bin.install "docker-manager"
+  end
+end
